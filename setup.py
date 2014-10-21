6ef80dcd19ddb091a987dde65cefa020ce9724b7
Daniel Neuhäuser <ich@danielneuhaeuser.de>
2014-10-21 14:52:55 +0200

    Bump to 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='pratt',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/DasIch/pratt',
     author='Daniel Neuhäuser',
     author_email='ich@danielneuhaeuser.de',
