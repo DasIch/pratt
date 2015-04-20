de75f5df53490418d3eb1b5d28d05bd76f5510ee
Daniel Neuhäuser <ich@danielneuhaeuser.de>
2015-04-20 10:47:02 +0200

    Add description to setup.py
    The long description is using the README.rst

--- setup.py
+++ setup.py
@@ -25,9 +25,20 @@ def get_version():
     return version
 
 
+def get_long_description():
+    readme_path = os.path.join(
+        os.path.dirname(__file__),
+        'README.rst'
+    )
+    with open(readme_path, encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
     name='pratt',
     version=get_version(),
+    description='A library for creating pratt parsers',
+    long_description=get_long_description(),
     url='https://github.com/DasIch/pratt',
     author='Daniel Neuhäuser',
     author_email='ich@danielneuhaeuser.de',
